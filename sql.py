d2d545606a3be01791b1a8e7c673aa8bfd1a8f6c
Your Name <you@example.com>
2019-03-02 12:51:18 +0530

    Final commit for Flask-blog App

---
+++ sql.py
@@ -0,0 +1,25 @@
+# sql.py - Create a SQLite3 table and populate it with data
+import sqlite3
+
+# create a new database if the database doesn't already exist
+with sqlite3.connect("blog.db") as connection:
+
+    # get a cursor object used to execute SQL commands
+    c = connection.cursor()
+
+    # create the table
+    c.execute("DROP TABLE if exists posts")
+    c.execute("""CREATE TABLE posts
+    (title TEXT, post TEXT)
+    """)
+
+    # insert dummy data into the table
+    c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
+    c.execute('INSERT INTO posts VALUES("Well", "I\'m well.")')
+    c.execute('INSERT INTO posts VALUES("Excellent", "I\'m excellent.")')
+    c.execute('INSERT INTO posts VALUES("Okay", "I\'m okay.")')
+
+    c.execute("Select * from posts")
+    posts = c.fetchall()
+    for post in posts:
+        print(post)
